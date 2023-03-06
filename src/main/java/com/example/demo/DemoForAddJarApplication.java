8e7f591692165d12ef6db621064035917e0864aa
JsonSong <hunt0101@163.com>
2023-03-06 22:37:53 +0800

    2

--- src/main/java/com/example/demo/DemoForAddJarApplication.java
+++ src/main/java/com/example/demo/DemoForAddJarApplication.java
@@ -7,22 +7,18 @@ import utils.JarLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @SpringBootApplication
 public class DemoForAddJarApplication {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		SpringApplication.run(DemoForAddJarApplication.class, args);
-		File file = new File("G:\\work\\java\\moreJar\\autoprint-1.0-SNAPSHOT.jar");
-//		System.out.println(file.toURI().toURL());
-//		ca.addToClassPath(file);
-		JarLoader.addToClassPath(file);
-		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-		Class<?> classz = classLoader.loadClass("autoprint.Model.PrintLine");
-		Method method = classz.getDeclaredMethod("printSuccess");
-		method.invoke(classz);
+	public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+		Class<?> clazz = Class.forName("Model.PrintLine");
+		Method method = clazz.getDeclaredMethod("printSuccess");
+		Constructor<?> ctor = clazz.getConstructor();
+		method.invoke(ctor.newInstance());
 	}
 
 }
